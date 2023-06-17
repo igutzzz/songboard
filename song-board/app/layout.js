5489be7767b0df8de43125d7f73c01aae6efd44a
Igor de Almeida <ofc.iguituu@gmail.com>
2023-06-17 18:35:09 -0300

    feat: ajuste layout e pagina da musica

--- song-board/app/layout.js
+++ song-board/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className + " bg-white"}>
+      <body className={inter.className + " bg-white flex"}>
         <Menu />
         {children}
         </body>
---
+++ song-board/app/song/page.js
@@ -0,0 +1,7 @@
+export default function Song() {
+    return(
+        <main className="px-32 py-16">
+            <h1>Projeto 1</h1>
+        </main>
+    )
+}
